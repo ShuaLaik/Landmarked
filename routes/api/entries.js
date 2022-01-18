cba7d98790f0107d71fbd6ca6474a3a9fdc43f97
Josh <joshualaikowski@gmail.com>
2022-01-18 10:32:24 -0500

    Fixed Main

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Entry = require('../../models/Entry');
 const validateEntryInput = require('../../validation/entry');
 
-const multer = require('multer')              // multer will be used to handle the form data.
-const Aws = require('aws-sdk')                // aws-sdk library will used to upload image to s3 bucket.
-// require("dotenv/config")                      // for using the environment variables that stores the confedential information.
+// const multer = require('multer')              // multer will be used to handle the form data.
+// const Aws = require('aws-sdk')                // aws-sdk library will used to upload image to s3 bucket.
+// // require("dotenv/config")                      // for using the environment variables that stores the confedential information.
 
 
 router.get(`/user/:user_id`, (req, res) => { // given user ID in params, returns all user's entries
@@ -51,47 +51,47 @@ router.delete('/:entry_id', (req, res) => { // given ENTRY ID, deletes entry,
 
 // AWS START
 
-const storage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-        cb(null, '')
-    }
-})
+// const storage = multer.memoryStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, '')
+//     }
+// })
 
-const filefilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
+// const filefilter = (req, file, cb) => {
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+//         cb(null, true)
+//     } else {
+//         cb(null, false)
+//     }
+// }
 
-const upload = multer({ storage: storage, fileFilter: filefilter });
+// const upload = multer({ storage: storage, fileFilter: filefilter });
 
-const s3 = new Aws.S3({
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID,              // accessKeyId that is stored in .env file
-    secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       // secretAccessKey is also store in .env file
-})
+// const s3 = new Aws.S3({
+//     accessKeyId:process.env.AWS_ACCESS_KEY_ID,              // accessKeyId that is stored in .env file
+//     secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       // secretAccessKey is also store in .env file
+// })
+//upload.single('productimage')
 
-
-router.post(`/`, upload.single('productimage'), (req, res) => { // given data object, creates new entry
+router.post(`/`, (req, res) => { // given data object, creates new entry
     const { errors, isValid } = validateEntryInput(req.body);
     
     if (!isValid) {
         return res.status(400).json(errors);
     }
     
-    const params = {
-        Bucket:process.env.AWS_BUCKET_NAME,      // bucket that we made earlier
-        Key:req.file.originalname,               // Name of the image
-        Body:req.file.buffer,                    // Body which will contain the image in buffer format
-        ACL:"public-read-write",                 // defining the permissions to get the public link
-        ContentType:"image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
-    };
+    // const params = {
+    //     Bucket:process.env.AWS_BUCKET_NAME,      // bucket that we made earlier
+    //     Key:req.file.originalname,               // Name of the image
+    //     Body:req.file.buffer,                    // Body which will contain the image in buffer format
+    //     ACL:"public-read-write",                 // defining the permissions to get the public link
+    //     ContentType:"image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
+    // };
     
-    s3.upload(params,(error,data)=>{
-        if(error){
-            res.status(500).send({"err":error})  // if we get any error while uploading error message will be returned.
-        }
+    // s3.upload(params,(error,data)=>{
+    //     if(error){
+    //         res.status(500).send({"err":error})  // if we get any error while uploading error message will be returned.
+    //     }
         
         
         
@@ -105,7 +105,7 @@ router.post(`/`, upload.single('productimage'), (req, res) => { // given data ob
         });
         newEntry.save()
         .then(entry => res.json(entry))
-    });
+    // });
 })
 
 
