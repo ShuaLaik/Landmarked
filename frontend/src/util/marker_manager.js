3524adc8d43834fb4e41c350a607035d8b5993d9
Mackinjosh <mackzumarraga@gmail.com>
2022-01-18 23:51:22 -0500

    polyline

--- frontend/src/util/marker_manager.js
+++ frontend/src/util/marker_manager.js
@@ -4,6 +4,7 @@ export default class MarkerManager {
 constructor(map){
       this.map = map;
       this.markers = {};
+      this.coords = [];
     }
   
     updateMarkers(entrys){
@@ -14,6 +15,14 @@ constructor(map){
 
         let clearCurrent = Object.keys(this.markers).filter(entryId => !entrysObj[entryId])
         clearCurrent.forEach((currentId) => this.removeMarker(this.markers[currentId]))
+
+        Object.values(entrys).forEach(entry => this.coords.push({
+            lat: parseFloat(entry.location.latitude),
+            lng: parseFloat(entry.location.longitude)
+            })
+        )
+
+        this.addPolyLine();
     };
 
     createMarkerFromListing(entry) {
@@ -28,6 +37,34 @@ constructor(map){
     removeMarker(marker) {
       this.markers[marker.entryId].setMap(null);
       delete this.markers[marker.entryId];
+    }
+
+    addPolyLine() {
+
+        const lineSymbol = {
+            path: "M 0,-1 0,1",
+            strokeOpacity: 1,
+            scale: 4,
+        };
+
+        var line = new window.google.maps.Polyline({
+          path: this.coords,
+          geodesic: true,
+          strokeColor: '#FF0000',
+        //   strokeOpacity: 1.0,
+          strokeOpacity: 0,  
+        //   strokeWeight: 2
+          icons: [
+            {
+              icon: lineSymbol,
+              offset: "0",
+              repeat: "20px",
+            },
+          ],
+        });
+  
+        line.setMap(this.map)
+        debugger
     }
 }
   
