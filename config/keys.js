2dc7fa6575e08125c8b634df1b5f4842cfe3884a
Josh <joshualaikowski@gmail.com>
2022-02-04 11:31:30 -0500

    Added Keys

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+
+// module.exports = require('./keys_prod');
+module.exports = {
+    mongoURI: "mongodb+srv://new_user:YwOIEUuBWHsiZ0MA@cluster0.mbzpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "landmark",
+    API_KEY: 'AIzaSyBf2TQSdFmrbbiT_nVFspaXAQTS-PnT3cI'
+}
+
+//AWSAccessKeyId=AKIAWPPWTP6HHSM2GDGZ
+
+//AWSSecretKey=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
