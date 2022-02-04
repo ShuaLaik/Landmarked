d9c21364a2be58d5a766ddad9064fd33199cf805
Josh <joshualaikowski@gmail.com>
2022-02-04 11:43:10 -0500

    Removed important Files

--- routes/api/dotenv/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    AWS_ACCESS_KEY_ID: "AKIAWQBTVTNQUPVCIW4Y",
-    AWS_ACCESS_KEY_SECRET: "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",
-    AWS_BUCKET_NAME: "mern-landmarked-dev1"
-}
