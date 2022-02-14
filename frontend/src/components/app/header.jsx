83b2a334d34d57b313e19f7efb19d7637c7ebfd9
Josh <joshualaikowski@gmail.com>
2022-02-14 17:30:24 -0500

    Added a lot

--- frontend/src/components/app/header.jsx
+++ frontend/src/components/app/header.jsx
@@ -12,7 +12,7 @@ export default class Header extends Component {
             <div className="Header">
                 <h1 className="h1"><span>L</span>and<span>M</span>arked</h1>
                 <div className="group">
-                    <a target="_blank" href=""><img src="https://secure.gravatar.com/avatar/11598a1d430471c67b5a1787ce19feab?secure=true&size=300"/></a>
+                    <a target="_blank" href="https://shualaik.github.io/JoshuaLaikowski/"><img src="https://secure.gravatar.com/avatar/11598a1d430471c67b5a1787ce19feab?secure=true&size=300"/></a>
                     <a target="_blank" href=""><img src="https://cdn.substack.com/image/fetch/w_112,h_112,c_fill,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fcd1ac032-322a-4237-b68a-2eb765d92ffa_745x423.jpeg"/></a>
                     <h1>Meet the Developers</h1>
                     <a target="_blank" href="https://ypeikes18.github.io/portfolio/"><img src="https://photos.angel.co/users/14172841-medium_jpg?1643158632"/></a>
